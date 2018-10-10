1dd066fcde06debd47690daec42598566867ac8c
Alistair Zhu <tong.zhu@laserfiche.com>
2018-10-10 09:06:34 -0400

    Initial commit

---
+++ app/src/main/java/com/az/city7/CreateActivity.java
@@ -0,0 +1,14 @@
+package com.az.city7;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class CreateActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+    }
+
+}
---
+++ app/src/main/java/com/az/city7/MainActivity.java
@@ -0,0 +1,26 @@
+package com.az.city7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button bt1 = (Button) findViewById(R.id.button);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+
+                bt1.setText("Automatically created a listener!");
+
+            }
+        });
+    }
+}
