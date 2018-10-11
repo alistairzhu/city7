a9af90cb3e8fc61c5403dad9fc79dee01d8aaba3
alistairzhu <alistairzhu@gmail.com>
2018-10-11 05:59:06 -0400

    add Create notice input fields

--- app/src/main/java/com/az/city7/MainActivity.java
+++ app/src/main/java/com/az/city7/MainActivity.java
@@ -6,7 +6,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.aurora.myweb3j.util.Alice;
+import com.example.aurora.myweb3j.util.Web3jConstants;
+
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.Web3jFactory;
+import org.web3j.protocol.core.methods.response.EthCoinbase;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.web3j.protocol.http.HttpService;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.concurrent.ExecutionException;
+
 public class MainActivity extends AppCompatActivity {
+
+    static public Web3j web3j = null;
+    static String clientUrl = null;
+    static String[] accounts = new String[15];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(v.getContext(), CreateActivity.class );
                 startActivityForResult(intent, 0);
 
-               // bt1.setText("Automatically created a listener!");
+
 
             }
         });
